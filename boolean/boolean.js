55c9620345fb590ad1c75b7f48f85a406f3df6ae
Joaoof <joaodeus400@gmail.com>
2023-03-15 11:40:19 -0300

    metodos para retornar booleans

--- boolean/boolean.js
+++ boolean/boolean.js
@@ -1,2 +1,11 @@
 // booleans e comparações
 console.log(true, false, 'true', 'false')
+
+//Metodos podem retornar booleans
+const email = 'joaodios300@gmail.com'
+const includes = email.includes('@')
+const names = ['dio', 'roger', 'robert']
+const arrayIncludes = names.includes('roger')
+
+console.log(includes)
+console.log(includes)
